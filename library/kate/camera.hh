0ec222ca6fdd4fc373b4758d997d7090db4fcb9a
kateBea <zanetty54@gmail.com>
2023-03-25 21:38:27 +0100

    camera changes

--- library/kate/camera.hh
+++ library/kate/camera.hh
@@ -15,23 +15,36 @@
 // Third-Party Libraries
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
-// Project Libraries
-#include <input.hh>
-
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// Project Libraries
+#include <input.hh>
+
 namespace kate {
     class camera {
     public:
+        explicit camera() = default;
 
+        camera(const camera&) = delete;
+        camera& operator=(const camera&) = delete;
+
+        camera(camera&&) = delete;
+        camera& operator=(camera&&) = delete;
+
+        auto update() -> void;
 
 
     private:
-
+        
     };
+
+    // CAMERA IMPLEMENTATION
+    inline auto update() -> void {
+        glm::mat4 projection{ get_projection() };
+        glm::mat4 view{ get_view() };
+        glm::mat4 model{ glm::mat4(1.0) };
+    }
 }
 #endif // END CAMERA_HH
